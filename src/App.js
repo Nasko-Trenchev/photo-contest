80b4faa7d904ccc08a76598c6c4d0e8c67d38108
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-03-13 13:54:45 +0200

    Fixing some styles

--- src/App.js
+++ src/App.js
@@ -11,23 +11,23 @@ function App() {
       <main>
         <h1>Welcome to the Photo Contest!</h1>
         <p>Compete in one of the three categories.</p>
-        <div class="fullwrap">
+        <div className="fullwrap">
           <img src="https://images.pexels.com/photos/2922672/pexels-photo-2922672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-          <div class="fullcap">
+          <div className="fullcap">
             Animals<br /><br /><br />
             <button>View category</button>
           </div>
         </div>
-        <div class="fullwrap">
-          <img src="https://images.pexels.com/photos/3244513/pexels-photo-3244513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-          <div class="fullcap">
+        <div className="fullwrap">
+          <img src="https://images.pexels.com/photos/345522/pexels-photo-345522.jpeg?auto=compress&cs=tinysrgb&w=600" />
+          <div className="fullcap">
             Nature<br /><br /><br />
             <button>View category</button>
           </div>
         </div>
-        <div class="fullwrap">
+        <div className="fullwrap">
           <img src="https://images.pexels.com/photos/2706654/pexels-photo-2706654.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-          <div class="fullcap">
+          <div className="fullcap">
             Space <br /><br /><br />
             <button>View category</button>
           </div>
