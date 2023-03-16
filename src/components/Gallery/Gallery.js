4daabc5606ae6e2650d4e6041cd8f9bb53b9b65d
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-03-16 18:39:55 +0200

    Add overlay on the images

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -15,13 +15,34 @@ export default function Gallery(){
         <section>
           <div className={styles["box"]}>
             <img src="https://images.unsplash.com/photo-1558788353-f76d92427f16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFyZ2UlMjBkb2d8ZW58MHx8MHx8&w=1000&q=80" alt="Rank 1" />
+            <div className={styles["image-overlay"]}>
+                <h3>Photo Name</h3>
+                <p>Participants count:</p>
+                <p>Time left:</p>
+                <p>Contest Prize:</p>
+                <button>See details</button>
+            </div>
           </div>
           {/* TODO: Render the second and third pictures */}
           <div className={styles["box"]}>
             <img src="https://images.unsplash.com/photo-1558788353-f76d92427f16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFyZ2UlMjBkb2d8ZW58MHx8MHx8&w=1000&q=80" alt="Rank 2" />
+            <div className={styles["image-overlay"]}>
+                <h3>Photo Name</h3>
+                <p>Participants count:</p>
+                <p>Time left:</p>
+                <p>Contest Prize:</p>
+                <button>See details</button>
+            </div>
           </div>
           <div className={styles["box"]}>
             <img src="https://images.unsplash.com/photo-1558788353-f76d92427f16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFyZ2UlMjBkb2d8ZW58MHx8MHx8&w=1000&q=80" alt="Rank 3" />
+            <div className={styles["image-overlay"]}>
+                <h3>Photo Name</h3>
+                <p>Participants count:</p>
+                <p>Time left:</p>
+                <p>Contest Prize:</p>
+                <button>See details</button>
+            </div>
           </div>
         </section>
         <section id="#more">
