44d447dbf18c1010fd9c85b74df82777aa82623a
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-03-21 11:40:20 +0200

    Adjusting CSS for Details page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import * as ContestService from '../../services/ContestService';
 
+import styles from './Details.module.css'
+
 export default function Details() {
 
   const [currentPhoto, setCurrentPhoto] = useState({})
@@ -13,7 +15,6 @@ export default function Details() {
   useEffect(() => {
     ContestService.getImageDetails(photoId)
       .then(result => {
-        console.log(result)
         setCurrentPhoto(result);
       })
   }, [photoId])
@@ -23,40 +24,40 @@ export default function Details() {
   }
 
   return (
-    <div className="details-page">
-      <div className="photo-container">
-        <img src={currentPhoto.imageUrl} alt="Photo" className="photo" />
+    <main className={styles["details-page"]}>
+      <div className={styles["photo-container"]}>
+      <h1 className={styles["name"]}>Name : uploaded by:  </h1>
+        <img src={currentPhoto.imageUrl} alt="Photo" className={styles["photo"]}/>
       </div>
-      <div className="details-container">
-        <h1 className="name">{currentPhoto.contestName}</h1>
-        <div className="like-section">
-
-          {!like ?
+      <div className={styles["details-container"]}>
+        <h1 className={styles["name"]}>{currentPhoto.contestName}</h1>
+        <div className={styles["like-section"]}>
+          {!like ?  
             <>
               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Facebook_Like_button.svg/1024px-Facebook_Like_button.svg.png"
                 onClick={() => setLike(true)} />
               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png"
                 onClick={increaseLike} />
             </> : <p>Thank you for your vote!</p>}
-          {!like && <span className="like-count">{likeCount}</span>}
+          {!like && <span className={styles["like-count"]}>{likeCount}</span>}
         </div>
-        <div className="comment-section">
-          <h2 className="comment-heading">Comments</h2>
-          <ul className="comment-list">
-            <li className="comment">
-              <span className="comment-author">John Doe:</span> This is a great product!
+        <div className={styles["comment-section"]}>
+          <h2 className={styles["comment-heading"]}>Comments</h2>
+          <ul className={styles["comment-list"]}>
+            <li className={styles["comment"]}>
+              <span className={styles["comment-author"]}>John Doe:</span> This is a great product!
             </li>
-            <li className="comment">
-              <span className="comment-author">Jane Smith:</span> I love this product!
+            <li className={styles["comment"]}>
+            <span className={styles["comment-author"]}>Jane Smith:</span> I love this product!
             </li>
-          </ul>
-          <form className="comment-form">
-            <label for="comment" className="comment-label">Leave a Comment:</label>
-            <textarea id="comment" name="comment" className="comment-input" required></textarea>
-            <button type="submit" className="comment-button">Post Comment</button>
+          </ul>   
+          <form className={styles["comment-form"]}>
+            <label for="comment" className={styles["comment-label"]}>Leave a Comment:</label>
+            <textarea id="comment" name="comment" className={styles["comment-input"]} required></textarea>
+            <button type="submit" className={styles["comment-button"]}>Post Comment</button>
           </form>
         </div>
       </div>
-    </div>
+    </main>
   )
 }
