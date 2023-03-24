453191c2502a325481c7b567a414b7a6dd253749
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-03-24 12:58:04 +0200

    Fixing nav CSS

--- src/components/NavigationHeader/NavigationHeader.js
+++ src/components/NavigationHeader/NavigationHeader.js
@@ -2,25 +2,27 @@ import { userContext } from "../../contexts/userContext";
 import { NavLink } from "react-router-dom";
 import { useContext } from "react";
 
+import styles from './NavigationHeader.module.css';
+
 export default function NavigationHeader(){
 
   const {user} = useContext(userContext);
 
     return (
         <header>
-        <nav className="navbar__menu">
-          <div className="logo">
+        <nav className={styles["navbar__menu"]}>
+          <div className={styles["logo"]}>
           <img src="asd" alt="Photo"/>
           </div>
-        <ul className="navbar__list">
+        <ul className={styles["navbar__list"]}>
           {user.accessToken ?
           <>
-          <li><NavLink className="navbar__item" to="/">Home</NavLink></li>
-          <li><NavLink className="navbar__item" to="/galery">Galery</NavLink></li>
-          <li><NavLink className="navbar__item" to="/logout">Logout</NavLink></li></>
+          <li><NavLink className={styles["navbar__item"]} to="/">Home</NavLink></li>
+          <li><NavLink className={styles["navbar__item"]} to="/galery">Galery</NavLink></li>
+          <li><NavLink className={styles["navbar__item"]} to="/logout">Logout</NavLink></li></>
           :
-          <><li><NavLink className="navbar__item" to="/login">Login</NavLink></li>
-          <li><NavLink className="navbar__item" to="/register">Register</NavLink></li></>}
+          <><li><NavLink className={styles["navbar__item"]} to="/login">Login</NavLink></li>
+          <li><NavLink className={styles["navbar__item"]} to="/register">Register</NavLink></li></>}
           {user.email === "admin@abv.bg" && <li><NavLink className="navbar__item" to="/admin">Admin</NavLink></li>}
         </ul>
       </nav>
